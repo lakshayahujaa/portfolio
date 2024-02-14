1ea4bed2e237bd1f0aee57450436fce8ee5215f7
lakshay <lakshayahuja210703@gmail.com>
2024-02-14 15:05:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,18 +10,3 @@ var typeData = new Typed(".role", {
     backSpeed: 80,
     backDelay: 1000,
   });
- function sendemail(){
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "lakshayahuja210703@gmail.com",
-        Password : "D60B2CDE91A2654C01B143BB35664FEF7A2A",
-        To : "lakshayahuja210703@gmail.com",
-        From : document.getElementById("email").value,
-        Subject : document.getElementById("subject").value,
-        Body : "Name:" + document.getElementById("name").value
-          +    "<br> Email: " + document.getElementById("email").value
-          +    "<br> Message: " + document.getElementById("message").value
-    }).then(
-      message => alert("Message sent Successfully")
-    );
- }
