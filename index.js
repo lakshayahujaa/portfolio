c916f4e2aa52ee4f0f8fa3c2c964d4537bc2a3ff
lakshay <lakshayahuja210703@gmail.com>
2024-02-21 13:19:47 +0530

    updated navbar

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ var typeData = new Typed(".role", {
     backSpeed: 80,
     backDelay: 1000,
   });
+
+var project = document.getElementsByClassName('nav-projects');
+var skills = document.getElementsByClassName('nav-skills');
+var contact = document.getElementsByClassName('nav-contact');
+
+function toProject(){
+  window.location.href = "https://lakshayahujaa.github.io/portfolio/#projects";
+  console.log("button clicked");
+}
+
+project.onclick = toProject;
